1892dc4c5d20912827a201093006eb5e3fa0d7dc
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-08-16 20:56:13 +0200

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,27 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        The bank has more than 800 electronically integrated local branches
+        located nationwide to provide the best and most accessible services to
+        customers.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
